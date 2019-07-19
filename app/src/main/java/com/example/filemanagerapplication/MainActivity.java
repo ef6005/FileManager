35407a638c75cdc1abc5c1eb95c282f5bec7dd77
ef6005 <ef6005@gmail.com>
2019-07-20 01:41:37 +0430

    emptyState is ready, begin fragments

--- app/src/main/java/com/example/filemanagerapplication/MainActivity.java
+++ app/src/main/java/com/example/filemanagerapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,22 +20,20 @@ public class MainActivity extends AppCompatActivity {
     public Button givePermissionBtn;
     public TextView emptyStateTv;
     public TextView titleBarTv;
-
+    public ViewGroup emptyStateViewGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //find views
-        emptyStateTv = findViewById(R.id.main_tv_emptyState);
         givePermissionBtn = findViewById(R.id.main_btn_givePermission);
         titleBarTv = findViewById(R.id.main_tv_titleBar);
+        emptyStateViewGroup = findViewById(R.id.main_cl_emptyStateGroup);
 
         //init views
         givePermissionBtn.setOnClickListener(this::beginRequestPermission);
-        //TODO:### ui empty state components
-//        emptyStateTv.setVisibility(View.GONE);
-//        givePermissionBtn.setVisibility(View.GONE);
+        emptyStateViewGroup.setVisibility(View.GONE);
 
         beginRequestPermission(null);
         //do nothing here that needs permission
@@ -52,13 +51,18 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == WRITE_PERMISSION_REQUEST_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //do not show permission emptyState
+                //TODO:### show another empty staye if needed
+                emptyStateViewGroup.setVisibility(View.GONE);
                 beginActivity();
+                return;
+            } else {//show emptyState
+                emptyStateViewGroup.setVisibility(View.VISIBLE);
+                return;
             }
-        } else {//show emptyState
-//            emptyStateTv.setVisibility(View.VISIBLE);
-//            givePermissionBtn.setVisibility(View.VISIBLE);
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private void beginActivity() {
